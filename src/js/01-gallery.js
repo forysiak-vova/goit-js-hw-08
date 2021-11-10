aad9d7f34e1514a5b000662eb4f902d50a397e02
forysiak-vova <volodja.forus@gmail.com>
2021-11-10 23:17:10 +0100

    json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,68 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(SimpleLightbox);
+
+
+import { galleryItems } from './gallery-items';
+
+console.log(JSON.stringify(galleryItems));
+
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', ongalleryContainerClick);
+
+function createImgCardsMarkup(galleryItems) {
+   return galleryItems.map(({preview, original}) => {
+   
+      return  `
+   <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image description"
+    />
+  </a>
+</div>
+   `
+   }).join('');
+   
+}
+
 console.log(galleryItems);
+
+function ongalleryContainerClick(e) {
+   const isGalleryClass = e.target.classList.contains('.gallery__image');
+   if (!isGalleryClass) {
+      return;
+   }
+   console.log(e.target);
+   
+};
+
+// const instance = basicLightbox.create(`
+// //     <img src=${original} width="800" height="600">
+// // `)
+
+// instance.show()
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img", //Переключение на имидж
+  captionsData: "alt", //Получение текста из Алт
+  captionPosition: "bottom", //Подпись внизу
+  captionDelay: 250, //Задержка 250 мс
+  showCounter: false, //Счетчик выключен
+  scrollZoom: false, //Масштабирование скролом отключено
+});
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+const formData = { };
+const refs = {
+   form: document.querySelector('.feedback-form'),
+   textarea: document.querySelector('.feedback-form textarea'),
+   input: document.querySelector('.feedback-form input')
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormInput);
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+   const resData = new FormData(e);
+   resData.array.forEach(element => {
+      
+   });
+  
+  
+};
+populateTexterea();
+function onFormSubmit(e) {
+   e.preventDefault();
+   const feedbackForm = localStorage.getItem('feedback-form-state');
+   const parsForm = JSON.parse(feedbackForm);
+   console.log(parsForm);
+
+   // console.log(localStorage.getItem('feedback-form-state'));
+   e.currentTarget.reset();
+   localStorage.removeItem('feedback-form-state');
+};
+
+function populateTexterea(e) {
+   const savedMessage = localStorage.getItem('feedback-form-state');
+   const localString = JSON.parse(savedMessage);
+   console.log(localString);
+
+    if (savedMessage) {
+      
+      refs.textarea.value = savedMessage;
+     refs.input.value = savedMessage;
+     
+   };
+ 
+};
